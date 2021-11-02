f40431fd6d1b1c3c6dae1809248196056500e283
Ininit <meininit@gmail.com>
2021-11-02 23:13:37 +0800

    init

---
+++ packages/vue/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@ininit/eslint-config-ts',
+  ],
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+  rules: {
+    'vue/max-attributes-per-line': ['warn', { singleline: 5 }],
+    'vue/html-self-closing': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/require-default-prop': 'off',
+  },
+}
